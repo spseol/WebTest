68c85f6fd1b85846e1df9fc44322b793b7982f3c
Tlapička <marek@tlapicka.net>
2015-03-10 14:12:18 +0100

    Vývojový http server
    Vývojový http server umožňuje při spuštění vybrat port a případně IP.

--- devtools/devserver.zsh
+++ devtools/devserver.zsh
@@ -2,10 +2,13 @@
 # Soubor:  devserver.zsh
 # Autor:   Marek Nožka, marek <@T> tlapicka <dot> net
 # Licence: GNU/GPL 
-############################################################
+###########################################################
+
+PORT=8888
 
 cd $(dirname $0)/..
 
 source ./venv/bin/activate
-python ./webtest.py
+{ sleep 2; x-www-browser "http://localhost:$PORT" }&
+python ./webtest.py $PORT 
 deactivate
